8b6046f92d148c056c09226f6cf8979214b791c8
amarjit-s-sahota <amarjit.s.sahota@gmail.com>
2018-11-25 14:14:39 -0800

    Add files via upload

---
+++ SuitBazaar/SuitBazaar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuitBazaar
+//
+//  Created by amarjit singh on 11/25/18.
+//  Copyright © 2018 amarjit singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SuitBazaar/SuitBazaarTests/SuitBazaarTests.swift
@@ -0,0 +1,34 @@
+//
+//  SuitBazaarTests.swift
+//  SuitBazaarTests
+//
+//  Created by amarjit singh on 11/25/18.
+//  Copyright © 2018 amarjit singh. All rights reserved.
+//
+
+import XCTest
+@testable import SuitBazaar
+
+class SuitBazaarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
